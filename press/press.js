0d76642f2ea0f2c40e28e04957836897688cec85
Ilona Brand <theilonabrand@gmail.com>
2024-04-09 14:40:23 -0500

    Press: move the empty state poem to be the third one

--- press/press.js
+++ press/press.js
@@ -1,13 +1,15 @@
 import { ProsePlay } from "proseplay";
 
-const poem = `Press( |ure|ing juice|ume)[1]
+const poem = `Press(ure|ing juice| |ume)[1]
 
 
-( |Absently|Anxiously|Patiently)[1] waiting for ( |cracks to show through|you to come through.|me to come to)[1]
+(Absently|Anxiously| |Patiently)[1] waiting for (cracks to show through|you to come through.| |me to come to)[1]
 
-( |Pressure creates the environment for the rarest jewels|Pressing distances me from you|Presume that we each had a story that was true)[1]
-( |but I was long checked out of our hotel room|but fuck it I set the table for two|that sometimes it takes years for softness to bloom)[1]
+(Pressure creates the environment for the rarest jewels|Pressing distances me from you| |Presume that we each had a story that was true)[1]
+(but I was long checked out of our hotel room|but fuck it I set the table for two| |that sometimes it takes years for softness to bloom)[1]
 
-( |Pl| Oranges fall to the ground as I rel| Back then, I folded flower after flower, found myself in each cr)[1]ease ( |Do Not Disturb| | e)[1]`;
+(Pl| Oranges fall to the ground as I rel| |Back then, I folded flower after flower, found myself in each cr)[1]ease (Do Not Disturb| | | e)[1]`;
 
-ProsePlay.parse(poem);
+const container = document.querySelector(".text");
+const pp = new ProsePlay(container);
+pp.parse(poem);
