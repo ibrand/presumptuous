db6ac1ac9158070bc02c1fa8381af44544bbefd1
Ilona Brand <theilonabrand@gmail.com>
2024-04-23 20:10:12 -0500

    Updates

---
+++ press/index.c63a7d7b.js
@@ -0,0 +1,2 @@
+let e=0;const t=["pressure","pressing-juice","press","pressume"];document.querySelector(".text"),document.querySelector(".next").addEventListener("click",c=>{let l=document.querySelectorAll(`.${t[e]}`)[0];l.style.opacity=0,e=(e+1)%t.length,(l=document.querySelectorAll(`.${t[e]}`)[0]).style.opacity=1});
+//# sourceMappingURL=index.c63a7d7b.js.map
