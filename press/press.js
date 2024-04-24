915998e33c7e9cca2a3f1e704beb49fe813e40bb
Ilona Brand <theilonabrand@gmail.com>
2024-04-23 20:08:19 -0500

    take proseplay out of the code -- still need to take out the packages

--- press/press.js
+++ press/press.js
@@ -1,15 +1,13 @@
-import { ProsePlay } from "proseplay";
-
-const poem = `Press(ure|ing juice| |ume)[1]
-
-
-(Absently|Anxiously| |Patiently)[1] waiting for (cracks to show through|you to come through.| |me to come to)[1]
-
-(Pressure creates the environment for the rarest jewels|Pressing distances me from you| |Presume that we each had a story that was true)[1]
-(but I was long checked out of our hotel room|but fuck it I set the table for two| |that sometimes it takes years for softness to bloom)[1]
-
-(Pl| Oranges fall to the ground as I rel| |Back then, I folded flower after flower, found myself in each cr)[1]ease (Do Not Disturb| | | e)[1]`;
-
+let currentPoemIndex = 0;
+const POEMS = ['pressure', 'pressing-juice', 'press', 'pressume'];
 const container = document.querySelector(".text");
-const pp = new ProsePlay(container);
-pp.parse(poem);
+
+document.querySelector(".next").addEventListener("click", (event) => {
+    let currentPoemEl = document.querySelectorAll(`.${POEMS[currentPoemIndex]}`)[0] // [0] because this is a NodeList
+    currentPoemEl.style.opacity = 0;
+
+    // then advance to the next poem
+    currentPoemIndex = (currentPoemIndex+1) % POEMS.length
+    currentPoemEl = document.querySelectorAll(`.${POEMS[currentPoemIndex]}`)[0] // [0] because this is a NodeList
+    currentPoemEl.style.opacity = 1;
+});
